92f87ca53396890c637b6cb74e7d59b01a6d4970
ballack84 <haitham.barqawi84@gmail.com>
2025-05-10 23:39:41 +0400

    update

--- src/MyPachage/Finaltestcases.java
+++ src/MyPachage/Finaltestcases.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -24,7 +25,7 @@ public class Finaltestcases extends dataparameters{
 	public void checkdefualtlangenglish() {
 	
 		String actuallang = driver.findElement(By.tagName("html")).getDomAttribute("lang");
-		Assert.assertEquals(actuallang, expectedlang);
+		Assert.assertEquals(actuallang, expectedlangen);
 		
 	}
 	
@@ -78,6 +79,57 @@ public class Finaltestcases extends dataparameters{
 
 	
 	
+	
+}
+@Test(priority=7)
+
+public void checkreturnedate() {	
+List<WebElement> dates =  driver.findElements(By.cssSelector(".sc-dXfzlN.iPVuSG"));
+String actualreturnedate = dates.get(1).getText();
+Assert.assertEquals(actualreturnedate,aftertomorrow);
+
+
+}
+@Test(priority=8)
+
+public void checkrandonlywebsitelanguage() throws InterruptedException {
+	
+	driver.get(website[randomindexforwebsite]);
+	
+	WebElement hoteltab = driver.findElement(By.xpath("//a[@data-testid='Header__HotelsNavigationTab']/span"));
+	hoteltab.click();
+
+	WebElement searchcities = driver.findElement(By.cssSelector(".sc-phbroq-2.uQFRS.AutoComplete__Input"));
+	
+	
+	if (driver.getCurrentUrl().contains("en")) {
+		
+		String actuallang = driver.findElement(By.tagName("html")).getDomAttribute("lang");
+		Assert.assertEquals(actuallang, expectedlangen);
+		searchcities.sendKeys(englishcities[randomenglishcities]);
+		WebElement listofcities = driver.findElement(By.cssSelector(".sc-phbroq-4.gGwzVo.AutoComplete__List"));
+		listofcities.findElements(By.tagName("li")).get(1);
+			
+	}
+	else {
+		String actuallang =  driver.findElement(By.tagName("html")).getDomAttribute("lang");
+		Assert.assertEquals(actuallang, expectedlangar);
+		searchcities.sendKeys(arabiccities[randomarabiccities]);
+		WebElement listofcities = driver.findElement(By.cssSelector(".sc-phbroq-4.gGwzVo.AutoComplete__List"));
+		listofcities.findElements(By.tagName("li")).get(1);
+	}
+	
+	WebElement numberofvisitors = driver.findElement(By.cssSelector(".sc-tln3e3-1.gvrkTi"));
+	Select myselector = new Select(numberofvisitors);
+	int randominex = rand.nextInt(2);
+	myselector.selectByIndex(randominex);
+	WebElement searchbutton = driver.findElement(By.cssSelector(".sc-1vkdpp9-5.btwWVk"));
+	searchbutton.click();
+	Thread.sleep(30000);
+	WebElement results = driver.findElement(By.xpath("//span[@data-testid='srp_properties_found']"));
+	results.getText();
+	Boolean actualresults = results.getText().contains("stays")||results.getText().contains("مكان");
+	Assert.assertEquals(actualresults,expectedresult);
 	
 }
 }
--- src/MyPachage/dataparameters.java
+++ src/MyPachage/dataparameters.java
@@ -2,6 +2,7 @@ package MyPachage;
 
 import java.time.Duration;
 import java.time.LocalDate;
+import java.util.Random;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -9,15 +10,25 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class dataparameters {
 	
 	WebDriver driver = new ChromeDriver();
+	Random rand = new Random();
 	String defualturl = "https://www.almosafer.com/en";
-	String expectedlang = "en";
+	String expectedlangen = "en";
+	String expectedlangar = "ar";
 	String excpectedcurrancy = "SAR";
 	String excpectedmobileno = "+966554400000";
 	Boolean excpetedqitaflogo = true;
 	String excpectedvaluehoteltab = "false";
 	LocalDate date = LocalDate.now();
 	int today = date.getDayOfMonth();
-	String tomorrow = Integer.toString(date.plusDays(1).getDayOfMonth());
+	String tomorrow = String.format("%02d",date.plusDays(1).getDayOfMonth());
+	String aftertomorrow = String.format("%02d",date.plusDays(2).getDayOfMonth());
+	String [] website = {"https://www.almosafer.com/en","https://www.almosafer.com/ar"};
+	int randomindexforwebsite = rand.nextInt(website.length);
+	String [] englishcities = {"Dubai","Jeddah","Riyadh"};
+	int randomenglishcities = rand.nextInt(englishcities.length);
+	String [] arabiccities = {"دبي","جده"};
+	int randomarabiccities = rand.nextInt(arabiccities.length);
+	Boolean expectedresult = true;
 
 
 
